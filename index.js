f2b57cb846fa14e51dbf44e151aa81d89adca79a
Caio Souza <113695603+Caiofz@users.noreply.github.com>
2023-11-19 02:08:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function ingredientesBolo(){
+    var ingredientes;
+    ingredientes = "INGREDIENTES\n\n1 xícara (chá) de açúcar de confeiteiro\n1½ colher (sopa) de caldo de laranja coado\n20 flores de mel comestíveis para decorar \n3 pedaços de casca de laranja cristalizada para decorar";
+    alert(ingredientes);
+}
+
+function ingredientesMacarrao(){
+    var ingredientes;
+    ingredientes = "INGREDIENTES\n\nBacon picado a gosto\nQueijo ralado a gosto\n3 ovos\nSal\nPimenta-do-reino a gosto\nMacarrão de sua escolha (espaguete, fusili,etc.)\nCreme de leite se quiser dar um toque diferente à receita";
+    alert(ingredientes);
+}
+
+function ingredientesPudim(){
+    var ingredientes;
+    ingredientes = "INGREDIENTES\n\n1 lata de leite condensado\nLeite\n1 lata de leite (medida da lata de leite condensado)\nOvo\n3 ovos inteiro\nCalda\nAçúcar\n1 xícara (chá) de açúcar\nÁgua\n1/2 xícara de água";
+    alert(ingredientes);
+}
